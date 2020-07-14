b8591dc96361521f515661cbe3335c28dcb78133
Luca Gasperini <luca.gasperini@xsoftware.it>
2020-07-14 15:33:10 +0200

    Added license on all files.

--- Sources/SeriousSamMF/ECS/PositionSystem.h
+++ Sources/SeriousSamMF/ECS/PositionSystem.h
@@ -1,3 +1,20 @@
+// Copyright (C) 2020 Luca Gasperini <luca.gasperini@xsoftware.it>
+//
+// This file is part of Serious Engine Reloaded.
+//
+// Serious Engine Reloaded is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Serious Engine Reloaded is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Serious Engine Reloaded.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef SSMF_POSITIONSYSTEM_H
 #define SSMF_POSITIONSYSTEM_H
 
--- Sources/SeriousSamMF/Types.h
+++ Sources/SeriousSamMF/Types.h
@@ -1,3 +1,23 @@
+// Copyright (C) 2020 Luca Gasperini <luca.gasperini@xsoftware.it>
+//
+// This file is part of Serious Engine Reloaded.
+//
+// Serious Engine Reloaded is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// Serious Engine Reloaded is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Serious Engine Reloaded.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef SER_TYPES_H
+#define SER_TYPES_H
+
 #include <stdint.h>
 /*
     Just a cool type definition standard
@@ -49,6 +69,8 @@ typedef uint_fast32_t INDEX;
 typedef uint_fast16_t PIX;
 typedef uint32_t COLOR;
 
+#endif
+
 /*
     Maybe define a standard for NAMES:
 
@@ -67,11 +89,20 @@ typedef uint32_t COLOR;
     File Name H:                    Pascal Case             (PositionSystem.h, Types.h, Manager.h)
     File Name UI:                   Pascal Case             (MainWindow.ui, etc...)
     Folder Names:                   Pascal Case             (SeriousSam, SeriousEditor, Bin)
-
+    Comment Fixme:                  FIXME(<EMAIL>, BugID):  FIXME(<luca.gasperini@xsoftware.it>, 111): Please fix this stuff!
+    Comment Todo:                   TODO(<EMAIL>, BugID):   TODO(<luca.gasperini@xsoftware.it>, 111): Please do this stuff!
     Static Variable:                (?)                     (?)
     Static Method:                  (?)                     (?)
     Static Member:                  (?)                     (?)
     Namespace:                      (?)                     (?)
 
+    Other cool RULES:
 
+    When define a pointer of a variable, place the "*" char on type name, not the variable name (example : VOID* myptr).
+    Float numbers end with "f" char and a number after point (example : 1.0f)
+    Try to stay on 80 char row length (Except for comment).
+    Use Inline comment to describe code.
+    Use Block comment to describe functions, class, struct, enum and method declaration.
+    Use Preprocessor Directives at the start of the line.
+    Namespaces have not extra level of indentation
 */
